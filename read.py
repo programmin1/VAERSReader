24ebc119eed08a7ab22df14e69a5a6001c5db4b5
lb1programmer <luke@linuxpc>
2021-11-24 01:28:26 -0800

    Count deaths by age group

--- read.py
+++ read.py
@@ -5,9 +5,18 @@ import os
 from collections import Counter
 from textblob import TextBlob
 def main(args):
+    THEMAX = 1E99
+    minage = 0
+    maxage = THEMAX
+    if( len(args) >2 ):
+        maxage = int(args[-1])
+        minage = int(args[-2])
+        print('Limiting to ages %s through %s' % (minage, maxage))
+        
     words = input('words to search: ')
     words = words.lower().split()
     nounphrases = []
+    died = 0
     #log:
     if not os.path.isdir('found'):
         os.mkdir('found')
@@ -25,14 +34,33 @@ def main(args):
                         if text.lower().find(w)==-1:
                             found = False; # Not found skip this record
                     if found:
-                        symptom = TextBlob(text).correct()
-                        print(symptom.noun_phrases)
-                        with open('found/'+row[3]+'yr-'+row[0]+'-'+row[6], 'w') as outfile:
-                            outfile.write(text)
-                        nounphrases += symptom.noun_phrases
+                        #print('age'+row[3])
+                        #if(row[3] == ''):
+                        #    print('unspecified age')
+                        #    print(row)
+                        if minage == 0 and maxage == THEMAX:
+                            #symptom = TextBlob(text).correct()
+                            #print(symptom.noun_phrases)
+                            with open('found/'+row[3]+'yr-'+row[0]+'-'+row[6], 'w') as outfile:
+                                outfile.write(text)
+                            nounphrases += text.split()
+                            if row[9] == 'Y':
+                                #print(row)
+                                died += 1
+                            
+                        elif row[3] != '' and float(row[3]) >= minage and float(row[3]) <= maxage:
+                            #symptom = TextBlob(text).correct()
+                            #print(symptom.noun_phrases)
+                            with open('found/'+row[3]+'yr-'+row[0]+'-'+row[6], 'w') as outfile:
+                                outfile.write(text)
+                            nounphrases += text.split()
+                            if row[9] == 'Y':
+                                #print(row)
+                                died += 1 
                 linecount += 1
                 #if linecount > 1000: #quick run?
                 #    break;
+    print('Died:%s' % (died,))
     c = Counter(nounphrases)
     for item in c.most_common(200):
         print(item)
